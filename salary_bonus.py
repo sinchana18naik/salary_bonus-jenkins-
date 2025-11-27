f693a03486edf608edf1d7c58006d4b311ec26bb
sinchana18naik <sinchananaik112005@gmail.com>
2025-11-27 14:57:06 +0530

    Update salary_bonus.py

--- salary_bonus.py
+++ salary_bonus.py
@@ -1,11 +1,11 @@
 import sys
 if len(sys.argv)==2:
   script_name=sys.argv[0]
-emp_salary=float(sys.argv[1])
+  emp_salary=float(sys.argv[1])
 else:
-emp_salary=50000
+  emp_salary=50000
 
-bonus=emp_salary*10
+bonus=emp_salary*0.10
 total=bonus+emp_salary
 print("salary before bonus:",emp_salary)
 print(" bonus given:",bonus)
