00407353e874e2c4c1d101afb782cd23e2036d3a
sinchana18naik <sinchananaik112005@gmail.com>
2025-11-27 14:45:27 +0530

    Create salary_bonus.py

---
+++ salary_bonus.py
@@ -0,0 +1,12 @@
+import sys
+if len(sys.argv)==2:
+  script_name=sys.argv[0]
+emp_salary=float(sys.argv[1])
+else:
+emp_salary=50000
+
+bonus=emp_salary*10
+total=bonus+emp_salary
+print("salary before bonus:",emp_salary)
+print(" bonus given:",bonus)
+print("salary after bonus:",total)
